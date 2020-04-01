aea75b86ea657b4c564cd929d7cbc1cca974a583
m4tuzalix <49403646+m4tuzalix@users.noreply.github.com>
2020-04-02 01:07:37 +0200

    Update page_model.py

--- features/pages/page_model.py
+++ features/pages/page_model.py
@@ -1,7 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 
 class basePage(object):
     def __init__(self):
